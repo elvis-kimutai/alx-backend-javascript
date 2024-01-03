246b8f86dfe20eb24ef192c437e20a66e218449e
elvis-kimutai <cheruiyotkim17@gmail.com>
2024-01-03 18:53:40 +0000

    Keep every promise

--- 0x01-ES6_promise/1-promise.js
+++ 0x01-ES6_promise/1-promise.js
@@ -1,3 +1,11 @@
-export default function getResponseFromAPI() {
-  return Promise.resolve();
+function getResponseFromAPI() {
+  return new Promise((resolve, reject) => {
+      if (true) {
+        resolve();
+      } else {
+        reject();
+      }
+  });
 }
+
+export default getResponseFromAPI;
