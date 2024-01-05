f45c0a201862ec4f3cfe62a072e2de0f267069a0
elvis-kimutai <cheruiyotkim17@gmail.com>
2024-01-05 16:04:00 +0000

    makes classroo,

--- 0x02-ES6_classes/1-make_classrooms.js
+++ 0x02-ES6_classes/1-make_classrooms.js
@@ -1,10 +1,6 @@
 import ClassRoom from './0-classroom';
 
-// Function to initialize ClassRoom objects
-function initializeRooms() {
-  const room1 = new ClassRoom(19);
-  const room2 = new ClassRoom(20);
-  const room3 = new ClassRoom(34);
-
-  return [room1, room2, room3];
+export default function initializeRooms() {
+  const clsr = [new ClassRoom(19), new ClassRoom(20), new ClassRoom(34)];
+  return clsr;
 }
